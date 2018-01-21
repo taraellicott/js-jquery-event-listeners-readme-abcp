fc5dab7c5dbdfc3df3043274a70a061d3dd84084
Tara Ellicott <taraellicott@gmail.com>
2018-01-21 18:11:37 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,21 @@
 //define functions here
 
+function getIt() {
+  
+}
+
+function frameIt() {
+  
+}
+
+function pressIt() {
+  
+}
+
+function submitIt() {
+  
+}
+
 $(document).ready(function(){
 
 // call functions here
